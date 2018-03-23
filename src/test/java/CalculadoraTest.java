cb40c37d45864c70aa47d1a966b4bdd9d7981bd9
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2018-03-23 13:51:08 -0300

    Failing test for null value

---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kbig
+ */
+public class CalculadoraTest {
+    Calculadora calc;
+    public CalculadoraTest() {
+        calc = new Calculadora();
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void medianaTestNulo() {
+        assertEquals(0.0, calc.calcMediana(null), 0.01);
+    }
+}
